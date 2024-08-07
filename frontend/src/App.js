5164596c657259ec0056fb2a8a3d423ba75e6547
Su Yue <77612121+lsuyueees@users.noreply.github.com>
2024-08-07 16:55:56 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import About from "./routes/About";
 import Contact from "./routes/Contact";
 import Service from "./routes/Service";
+import TravelAdvisories from "./routes/TravelAdvisories";
 import { AuthProvider } from './AuthContext';
 
 export default function App() {
@@ -16,6 +17,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/service" element={<Service />} />
+        <Route path="/TravelAdvisories" element={<TravelAdvisories />} />
       </Routes>
       </AuthProvider>
       

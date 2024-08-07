f4086343de34b1374d2824d69896399270162bf9
Su Yue <77612121+lsuyueees@users.noreply.github.com>
2024-08-07 16:48:04 +0800

    Update Navbar.js
    Added a side menu on the nav bar, and moved all the nav bar menu items into the side menu. 
    The side menu also consists of additional list items, which were what we placed in high fidelity.
    Nav bar logo has been centralized, and linked to the home page

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,42 @@
 import { Component, useContext } from "react";
 import "./NavbarStyles.css";
-import { MenuItems } from "./MenuItems";
+// import { MenuItems } from "./MenuItems";
 import { Link } from "react-router-dom";
 import LoginRegister from "./LoginRegister";
-import  AuthContext  from "../AuthContext";
+import AuthContext from "../AuthContext";
+import * as FaIcons from "react-icons/fa";
+import styled from "styled-components";
+import Sidebar from "../components/Sidebar";
+
+const Nav = styled.div`
+    background: #ffffff;
+    height: 80px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    position: fixed;
+    z-index: 9999;
+`;
+
+const NavIcon = styled(Link)`
+    color: #7392a6;
+    margin-right: 2rem;
+    font-size: 2rem;
+    height: 80px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+`;
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+      showPopup: false,
+      sidebar: false,
+    };
+  }
   state = { clicked: false, showPopup: false };
 
   handleClick = () => {
@@ -16,44 +47,55 @@ class Navbar extends Component {
     this.setState({ showPopup: !this.state.showPopup });
   };
 
+  toggleSidebar = () => {
+    this.setState({ sidebar: !this.state.sidebar });
+  };
+
   render() {
     return (
       <AuthContext.Consumer>
         {({ isAuthenticated, logout }) => (
-          <nav className="NavbarItems">
-            <h1 className="navbar-logo">Trippy</h1>
+          <>
+            <nav className="NavbarItems">
+              <NavIcon to="#">
+                <FaIcons.FaBars onClick={this.toggleSidebar} />
+              </NavIcon>
+              <a className="navbar-logo" href="/"><h1>Trippy</h1></a>
 
-            <div className="menu-icons" onClick={this.handleClick}>
-              <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
-            </div>
-
-            <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-              {MenuItems.map((item, index) => {
-                return (
-                  <li key={index}>
-                    <Link className={item.cName} to={item.url}>
-                      <i className={item.icon}></i>
-                      {item.title}
-                    </Link>
-                  </li>
-                );
-              })}
-              {!isAuthenticated ? (
-                <button onClick={this.togglePopup}>Sign In</button>
-              ) : (
-                <button onClick={logout}>Log Out</button>
-              )}
-            </ul>
-            {this.state.showPopup && (
-              <div className="modal-overlay">
-                <div className="modal-content">
-                  <button className="close-btn" onClick={this.togglePopup}>×</button>
-                  <LoginRegister closePopup={this.togglePopup} />
-                </div>
+              <div className="menu-icons" onClick={this.handleClick}>
+                <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
               </div>
-            )}
-          </nav>
-        )}
+
+              <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+                {/* {MenuItems.map((item, index) => {
+                    return (
+                      <li key={index}>
+                        <Link className={item.cName} to={item.url}>
+                          <i className={item.icon}></i>
+                          {item.title}
+                        </Link>
+                      </li>
+                    );
+                  })} */}
+                {!isAuthenticated ? (
+                  <button onClick={this.togglePopup}>Sign In</button>
+                ) : (
+                  <button onClick={logout}>Log Out</button>
+                )}
+              </ul>
+              {this.state.showPopup && (
+                <div className="modal-overlay">
+                  <div className="modal-content">
+                    <button className="close-btn" onClick={this.togglePopup}>×</button>
+                    <LoginRegister closePopup={this.togglePopup} />
+                  </div>
+                </div>
+              )}
+            </nav>
+            <Sidebar sidebar={this.state.sidebar} toggleSidebar={this.toggleSidebar} />
+          </>
+        )
+        }
       </AuthContext.Consumer>
     );
   }

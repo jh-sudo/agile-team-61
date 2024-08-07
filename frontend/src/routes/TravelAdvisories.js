de280cc753605c2d6629d4cd1dcf6e52c6b440ea
Su Yue <77612121+lsuyueees@users.noreply.github.com>
2024-08-07 16:23:36 +0800

    Add files via upload

---
+++ frontend/src/routes/TravelAdvisories.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function TravelAdvisories() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
+
+export default TravelAdvisories;

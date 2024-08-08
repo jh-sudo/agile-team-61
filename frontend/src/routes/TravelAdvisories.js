ddea006b94c5189ad79b33247015b1e98046b27e
Su Yue <77612121+lsuyueees@users.noreply.github.com>
2024-08-08 18:06:36 +0800

    Update TravelAdvisories.js

--- frontend/src/routes/TravelAdvisories.js
+++ frontend/src/routes/TravelAdvisories.js
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import AdvisoriesData from '../components/AdvisoriesData';
+import ClinicData from '../components/ClinicData';
 
-function TravelAdvisories() {
+const TravelAdvisories = () => {
   return (
     <>
       <Navbar />
+      <AdvisoriesData />
+      <hr></hr>
+      <ClinicData />
+      <br></br>
       <Footer />
     </>
   );
-}
+};
 
 export default TravelAdvisories;

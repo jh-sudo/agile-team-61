87272762698d9bf2b9005e37417676bf2cd33ce6
Su Yue <77612121+lsuyueees@users.noreply.github.com>
2024-08-27 01:36:49 +0800

    Update AdvisoryAPI.js
    Removed unnecessary codes

--- frontend/src/components/AdvisoryAPI.js
+++ frontend/src/components/AdvisoryAPI.js
@@ -1,7 +1,3 @@
-import axios from 'axios';
-
-// const API_URL = 'https://www.travel-advisory.info/api';
-
 export const getTravelAdvisories = async () => {
   const response = await fetch('https://www.travel-advisory.info/api');
   if (!response.ok) {
